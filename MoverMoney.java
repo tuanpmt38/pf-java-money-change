841b0b9005a88846f42862ab43d21b79d251e00d
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-03-18 16:01:59 +0700

    [Bài tập] Ứng dụng chuyển đổi tiền tệ

--- MoverMoney.java
+++ MoverMoney.java
@@ -1,11 +1,15 @@
+import java.text.DecimalFormat;
+
 public class MoverMoney {
     public static void main(String[] args) {
         float vnd, usd;
         float rate = 23000;
+        String pattern = "###,###,###";
+        DecimalFormat df = new DecimalFormat(pattern);
         java.util.Scanner sc = new java.util.Scanner(System.in);
         System.out.println("Enter money USD: ");
         usd = sc.nextFloat();
         vnd = rate * usd;
-        System.out.println("Display money VND:" + vnd);
+        System.out.println("Display money VNĐ:" + df.format(vnd) + " VNĐ");
     }
 }
